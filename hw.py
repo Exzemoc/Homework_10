34370e47a6d2543f0f51fbd37bf530a02aa9978a
Vlad <120489891+Exzemoc@users.noreply.github.com>
2023-03-02 20:44:47 +0300

    hw10.1

--- hw.py
+++ hw.py
@@ -7,51 +7,53 @@ print('''Выберите операцию:
         6 - Деление по модулю
         7 - Целочисленное деление''')
 x = int(input())
-try:
-    if x == 1:
-        print('Сумма:')
-        a = int(input('a = '))
-        b = int(input('b = '))
-        c = a + b
-        print(c)
-    elif x == 2:
-        print('Разность:')
-        a = int(input('a = '))
-        b = int(input('b = '))
-        c = a - b
-        print(c)
-    elif x == 3:
-        print('Умножение:')
-        a = int(input('a = '))
-        b = int(input('b = '))
-        c = a * b
-        print(c)
-    elif x == 4:
-        print('Деление:')
-        a = int(input('a = '))
-        b = int(input('b = '))
-        c = a / b
-        print(c)
-    elif x == 5:
-        print('Возведение а в степень b:')
-        a = int(input('a = '))
-        b = int(input('b = '))
-        c = a**b
-        print(c)
-    elif x == 6:
-        print('Деление по модулю:')
-        a = int(input('a = '))
-        b = int(input('b = '))
-        c = a % b
-        print(c)
-    elif x == 7:
-        print('Целочисленное деление:')
-        a = int(input('a = '))
-        b = int(input('b = '))
-        c = a//b
-        print(c)
-except ValueError:
-    print('Invalid value')
-except ZeroDivisionError:
-    print('Divided by zero')
-
+if 1<=x<=7:
+    try:
+        if x == 1:
+            print('Сумма:')
+            a = int(input('a = '))
+            b = int(input('b = '))
+            c = a + b
+            print(c)
+        elif x == 2:
+            print('Разность:')
+            a = int(input('a = '))
+            b = int(input('b = '))
+            c = a - b
+            print(c)
+        elif x == 3:
+            print('Умножение:')
+            a = int(input('a = '))
+            b = int(input('b = '))
+            c = a * b
+            print(c)
+        elif x == 4:
+            print('Деление:')
+            a = int(input('a = '))
+            b = int(input('b = '))
+            c = a / b
+            print(c)
+        elif x == 5:
+            print('Возведение а в степень b:')
+            a = int(input('a = '))
+            b = int(input('b = '))
+            c = a**b
+            print(c)
+        elif x == 6:
+            print('Деление по модулю:')
+            a = int(input('a = '))
+            b = int(input('b = '))
+            c = a % b
+            print(c)
+        elif x == 7:
+            print('Целочисленное деление:')
+            a = int(input('a = '))
+            b = int(input('b = '))
+            c = a//b
+            print(c)
+    except ValueError:
+        print('Invalid value')
+    except ZeroDivisionError:
+        print('Divided by zero')
+else:
+    print('Incorrect value')
